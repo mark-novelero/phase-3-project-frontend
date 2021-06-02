2e5d6d96e62d11f01364231ea2a7e599bbf4857a
mark novelero <mark.novelero@gmail.com>
2021-06-02 17:59:05 -0400

    EOD 6/2

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import FixedHeader from './fixed_header/FixedHeader';
 import MainPage from './main_component/MainPage';
 import "./App.css"
 
-export default class App extends React.Component{
 
+
+export default class App extends React.Component{
   state = {
     photos: [], 
-    selectPhoto: []
+    selectPhoto: [],  
+    blogs: [] 
   }
 
   componentDidMount(){
@@ -21,16 +23,48 @@ export default class App extends React.Component{
            selectPhoto: photographs[randomIndex]
         }
       )
-    )    
+    )
+    
+      fetch('http://localhost:9292/blogs')
+      .then(res => res.json())
+      .then(userBlogs => this.setState(
+        {blogs: userBlogs}
+      ))
 }
 
 
+addNewBlog = (blogObj) =>{
+    let newBlog = {
+      user_ids: blogObj.user_ids, 
+      photo_ids: blogObj.photo_ids, 
+      story: blogObj.story 
+    }
+
+    fetch("http://localhost:9292/blogs", {
+     method: "POST",
+     headers: {
+     "Content-Type": "application/json",
+   },
+      body: JSON.stringify(newBlog),
+   })
+    .then (res => res.json())
+    .then (newBlogo => {
+    this.setState({
+      blogs: [...this.state.blogs, newBlogo]
+    })
+  })
+}
+
+
+
+
+
   render() {
+
     return (
       <div className= "main">
         <FixedHeader></FixedHeader>
-        <br></br>
-        <MainPage mainPhoto = {this.state.selectPhoto}></MainPage>
+        <MainPage mainPhoto = {this.state.selectPhoto} addNewBlog= {this.addNewBlog}></MainPage>
         <br></br>
       </div>
     )
